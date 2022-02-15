7483caed50e74d6b06ae89c6d64e325f45153450
Aman Singh <60505070+AmanGargvanshi@users.noreply.github.com>
2022-02-15 19:08:36 +0530

    Update webserver.py

--- Embedded System Based Fire Fighting Kit/src/webserver.py
+++ Embedded System Based Fire Fighting Kit/src/webserver.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template, Response
+from flask import Flask,render_template, Response,url_for,request,redirect,make_response
 from FireFighterVision import VideoCamera
+import json
+from time import time
+from sensorData import sensorData
+from sendMail import sendMail
 
+#initiate flask app
 app = Flask(__name__)
 
-@app.route('/')
+#render Home page template 
+@app.route('/home')
+def home():
+    return render_template('applicationPage.html')
+
+#render Login page template
+@app.route('/',methods=["GET","POST"])
 def index():
-    return render_template('index.html')
+    error = None
+    if request.method == "POST":
+        if request.form['username']!= "admin" or request.form['password'] != "admin":
+            error = "Invalid credentials"
+        else:
+            return redirect(url_for('home'))
+    return render_template('loginPage.html',error=error)
+
+#get sensors data in the flask server for plotting
+@app.route('/data', methods=["GET", "POST"])
+def data():
+    data = [time() * 1000,sensorData.HeartRate()]
+    response = make_response(json.dumps(data))
+    response.content_type = 'application/json'
+    return response
 
 def gen(camera):
     while True:
@@ -13,10 +38,14 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+#feeding video to flask app
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+#main 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="192.168.0.103",port = 5000, debug=True)
+    sendMail()
+
